c53f3419427df5a648050e6480a9fbce01d0f553
Maks Hayduk <maksym.h@nerdysoft.com>
2019-10-06 17:05:30 +0300

    Changed project theme, added jQuery, animations

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const getElementByClassName = (className) => document.getElementsByClassName(className);
-
-const setActive = (e) => {
-  const elements = getElementByClassName('nav-link');
-  if (e.target.classList.value.includes('active')) {
-    console.log('active')
-  }
-  elements.map(el => console.log(el))
-  console.log(elements);
+const navbarScroll = (scrollTop) => {
+  $('html, body').animate({
+    scrollTop: scrollTop
+  }, 1300);
 }
+
+$(document).ready(() => {
+  $('#bg').animate({height: `1000px`}, 1500);
+  $('#header').animate({opacity: `0.7`}, 1000);
+  $('html, body').animate({
+    scrollTop: 0
+  }, 0);
+});
+
+$("#general-info-button").click(() => {
+  navbarScroll($("#general-info-block").offset().top - 56);
+});
+
+$("#nav-barber-button").click(() => {
+  navbarScroll(0);
+});
+
+$(window).scroll(() => {
+  let scrollTop = $(window).scrollTop();
+  if (scrollTop > $('#general-info-block').offset().top - 400 && $('#bg').height() > 400) { 
+    $('#general-info-block').animate({opacity: 1}, 300)
+  }
+});
