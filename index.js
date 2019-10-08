a70d6ef4c2fbdd7124f6e1ea3bc0593450672a94
Maks Hayduk <maksym.h@nerdysoft.com>
2019-10-08 19:44:15 +0300

    Added general info page w/ animations

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const navbarScroll = (scrollTop) => {
 
 $(document).ready(() => {
   $('#bg').animate({height: `1000px`}, 1500);
-  $('#header').animate({opacity: `0.7`}, 1000);
+  $('#bg-text').animate({opacity: 1}, 1000);
+  
   $('html, body').animate({
     scrollTop: 0
   }, 0);
@@ -22,7 +23,32 @@ $("#nav-barber-button").click(() => {
 
 $(window).scroll(() => {
   let scrollTop = $(window).scrollTop();
-  if (scrollTop > $('#general-info-block').offset().top - 400 && $('#bg').height() > 400) { 
-    $('#general-info-block').animate({opacity: 1}, 300)
+
+  if (scrollTop > $('#bg').height()/4) {
+    $('#header').animate({opacity: `1`}, 1500)
+  }
+
+  if (scrollTop > $('#general-info-block').offset().top/2 && $('#bg').height() > $('#bg').height()/2) { 
+    $('#general-info-block').animate({opacity: 1}, 1000);
+
+    $("#first-info").animate({
+      opacity: 1,
+      left: "100px"
+    }, 2000);
+
+    $("#col-img-1").animate({
+      opacity: 1
+    }, 1000)
+
+    setTimeout(() => {
+      $("#second-info").animate({
+        opacity: 1,
+        left: "-100px"
+      }, 2000);
+  
+      $("#col-img-2").animate({
+        opacity: 1
+      }, 1000)
+    }, 1000)
   }
 });
